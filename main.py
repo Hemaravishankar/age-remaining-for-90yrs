ec5810d64d68c118a7eaea0786716552296b0232
Hemaravishankar <hematirumalla@gmail.com>
2020-11-30 07:22:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+age = int(input("Enter your age: "))
+
+years_remaining = 90 - age
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+
+#f_string
+
+message = f"you have {days_remaining} days, {weeks_remaining}weeks,and {months_remaining} months left."
+
+print(message)
